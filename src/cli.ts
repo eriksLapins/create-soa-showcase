cb6b6fc1543266703835b929f22d8c48941ac951
Ēriks Lapiņš <lapinseriks98@gmail.com>
2024-02-24 08:58:22 +0200

    addded prompts for template and packageManager

--- src/cli.ts
+++ src/cli.ts
@@ -10,13 +10,23 @@ const templateRoot = resolve(__dirname, '..', 'templates');
 create('create-soa-showcase', {
   templateRoot,
   extra: {
-    framework: {
+    template: {
       type: 'list',
+      default: 'nuxt',
       describe: 'choose your framework',
       choices: ['nuxt'],
       prompt: 'if-no-arg',
     },
+    packageManager: {
+      type: 'list',
+      default: 'yarn',
+      describe: 'choose your package manager',
+      choices: ['yarn', 'npm', 'pnpm', 'bun'],
+      prompt: 'if-no-arg',
+    },
   },
-  after: ({ answers }) => console.log(`Ok you chose ${answers.framework}.`),
+  after: ({ answers }) => {
+    console.log(`Installing dependencies for ${answers.template} template with ${answers.packageManager}.`);
+  },
   caveat: 'Project created',
 });
